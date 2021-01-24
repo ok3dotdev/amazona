91c0349157ea2c15349c1d520abe6f7d9915a8dd
sickerman <henryoke158@yahoo.com>
2021-01-23 22:00:46 -0500

    adding redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
