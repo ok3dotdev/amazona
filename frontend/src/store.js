913300d9487fc690d76bcf40448e3723c2425c12
sickerman <henryoke158@yahoo.com>
2021-01-24 18:25:52 -0500

    add paid with paypal

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,6 +4,7 @@ import { cartReducer } from "./reducers/cartReducers";
 import {
   orderCreateReducer,
   orderDetailsReducer,
+  orderPayReducer,
 } from "./reducers/orderReducers";
 import {
   productDetailsReducer,
@@ -38,6 +39,7 @@ const reducer = combineReducers({
   userSignin: userSigninReducer,
   userRegister: userRegisterReducer,
   orderCreate: orderCreateReducer,
+  orderPay: orderPayReducer,
   orderDetails: orderDetailsReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
