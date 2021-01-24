254fbca40308fd9604cd34383023de04c5458ac2
sickerman <henryoke158@yahoo.com>
2021-01-24 13:38:46 -0500

    MONDODB USERS

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
 import data from "./data.js";
+import userRouter from "./routers/userRouter.js";
 
-// eslint-disable-next-line no-undef
-const port = process.env.PORT || 5000;
 const app = express();
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amazona", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
 app.get("/api/products/:id", (req, res) => {
   const product = data.products.find((x) => x._id === req.params.id);
@@ -18,10 +23,16 @@ app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
 
+app.use("/api/users", userRouter);
 app.get("/", (req, res) => {
-  res.send("server is running");
+  res.send("Server is ready");
 });
 
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Serve at http://localhost:${port}`);
 });
